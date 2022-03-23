e8244c29653cb6a75364a977788009e5f0984bdc
shehani <shehaniperera63@gmail.com>
2022-03-23 23:33:11 +0530

    created blacklist page and adding payment details page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blacklist', function () {
+    return view('blacklist');
+});
+
+Route::get('/view', function () {
+    return view('view');
+});
